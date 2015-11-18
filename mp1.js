cf0ddb1066c5bbad1c532fb88897c1f7a20ddd6d
Christoph Willing <chris.willing@iinet.net.au>
2015-11-18 14:27:03 +1000

    Use separate websockets for status * profile pages (mp1 & mp2)

---
+++ mp1.js
@@ -0,0 +1,81 @@
+$(document).ready( function(){
+
+
+var profileLink = document.querySelector('#open_profiles');
+var gmyWin = null;
+var received = $('#received');
+
+//var socket = new WebSocket("ws://localhost:8080/ws");
+var socket = new WebSocket("ws://" + location.host + "/wsStatus");
+ 
+socket.onopen = function(){  
+  console.log("sss connected"); 
+};
+
+socket.onmessage = function (message) {
+
+  received.append(message.data);
+  received.append($('<br/>'));
+  received.scrollTop(received.prop('scrollHeight'));
+
+};
+
+socket.onclose = function(){
+  console.log("disconnected"); 
+};
+
+var sendMessage = function(message) {
+  console.log("sending:" + message.data);
+  socket.send(message.data);
+};
+
+    // GUI Stuff
+
+
+    // send a command to the serial port
+    $("#cmd_send").click(function(ev){
+      ev.preventDefault();
+      var cmd = $('#cmd_value').val();
+      sendMessage({ 'data' : cmd});
+      $('#cmd_value').val("");
+    });
+
+    $('#clear').click(function(){
+      received.empty();
+    });
+
+
+// Open Profiles page
+
+  profileLink.onclick = function() {
+    //gmyWin = myOpenWindow("/static/mp2.html", "Profiles", "height=0", gmyWin );
+    gmyWin = myOpenWindow("/profile", "Profiles", "height=0", gmyWin );
+    console.log('Profiles button clicked');
+  }
+
+//myOpenWindow from http://www.joemarini.com/tutorials/tutorialpages/window1.php
+function myOpenWindow(winURL, winName, winFeatures, winObj)
+{
+  var theWin; // this will hold our opened window
+console.log("XXX %s", winName);
+
+  // first check to see if the window already exists
+  if (winObj != null)
+  {
+    // the window has already been created, but did the user close it?
+    // if so, then reopen it. Otherwise make it the active window.
+    if (!winObj.closed) {
+      winObj.focus();
+      return winObj;
+    }
+    // otherwise fall through to the code below to re-open the window
+  }
+
+  // if we get here, then the window hasn't been created yet, or it
+  // was closed by the user.
+  theWin = window.open(winURL, winName, winFeatures);
+  return theWin;
+}
+
+
+});
